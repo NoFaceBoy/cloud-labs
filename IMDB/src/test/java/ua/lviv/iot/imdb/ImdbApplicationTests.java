3270857e0681f799ef776654e8f037bcee540b13
Oleksandr <telyuksasha@gmail.com>
2023-11-08 17:02:07 +0200

    Properties for application and tests fix

--- IMDB/src/test/java/ua/lviv/iot/imdb/ImdbApplicationTests.java
+++ IMDB/src/test/java/ua/lviv/iot/imdb/ImdbApplicationTests.java
@@ -3,7 +3,7 @@ package ua.lviv.iot.imdb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ImdbApplicationTests.class)
 class ImdbApplicationTests {
 
 	@Test
